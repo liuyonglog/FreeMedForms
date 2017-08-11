9e8566a8f95295e23feee1edbb5b62904829b4ea
LiuYong <liuyonglog@163.com>
2017-08-12 01:07:06 +0800

    commit bat file used qt4.8

---
+++ freemedforms/bulid/makeproject.bat
@@ -0,0 +1,2 @@
+D:\Qt\qt-4.8.6-x64-msvc2010\qt-4.8.6-x64-msvc2010\bin\qmake  ..\freemedforms.pro -tp vc -r "CONFIG+=debug debug_without_install" -spec win32-msvc2010  LOWERED_APPNAME=freediams
+pause
